8ae65a1d4d4182e8a5de59c67591c958f292e015
Flynn <flynndclark@gmail.com>
2017-08-21 09:11:24 +1000

    first commit

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component
+{
+
+  render()
+  {
+    return (
+      <div className="BusinessList">
+      {
+        this.props.businesses.map(function hi(business)
+      {
+        return <Business newProp={business} />
+      })
+      }
+      </div>
+
+    );
+  }
+
+}
+
+export default BusinessList;
