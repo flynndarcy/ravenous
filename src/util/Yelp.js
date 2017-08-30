26b5cc19d60c806630de7d1f5612ccb88333d1c8
Flynn <flynndclark@gmail.com>
2017-08-30 11:51:42 +1000

    Updated

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,22 +1,22 @@
 const clientId = 'luKfditnFxjs-XJpjNwCWQ';
 const secret = '8k7O9lJ0pfliwCvPoo9SGQscHbsEqwTYysnq6fKpfu9cdEn1FSVLGSXDlosraz45';
 
-let accessToken = '';
+let acessToken;
 
 let Yelp = {
 
   getAccessToken()
   {
-    if(this.acessToken)
+    if(acessToken)
     {
-      return new Promise(resolve => resolve(this.acessToken));
+      return new Promise(resolve => resolve(acessToken));
     }
 
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {method: 'POST'}).then(response => {
   return response.json();
 }).then(jsonResponse => {
 
-  this.access_token = jsonResponse.access_token;
+  access_token = jsonResponse.access_token;
 });
 
   },
@@ -37,26 +37,26 @@ let Yelp = {
 ).then(response => {
   return response.json();
 }).then(jsonResponse => {
-  if(jsonResponse.businesses) {
-
-    return jsonResponse.businesses.map(business => {
-     {
-        id: business.id,
-        imageSrc: business.imageSrc,
-        name: business.name,
-        address: business.address,
-        city: business.city,
-        state: business.state,
-        zipCode: business.zipCode,
-        category: business.category,
-        rating: business.rating,
-        reviewCount: business.reviewCount
-      });
-    }
-  });
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
 
 
 
+
+}
 
 }
 
